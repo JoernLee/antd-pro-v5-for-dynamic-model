4e87fe9ed080414ec23120d407a4575e572bd551
Joern <924901972@qq.com>
2022-02-24 22:19:04 +0800

    perf:improve modal form

--- src/pages/BasicList/Modal.tsx
+++ src/pages/BasicList/Modal.tsx
@@ -3,6 +3,7 @@ import { useRequest } from 'umi';
 import { useEffect } from 'react';
 import FormBuilder from '@/pages/BasicList/builder/FormBuilder';
 import ActionBuilder from '@/pages/BasicList/builder/ActionBuilder';
+import moment from 'moment';
 
 const Modal = ({
   visible,
@@ -13,13 +14,41 @@ const Modal = ({
   uri: string;
   handleCancel: () => void;
 }) => {
+  const [form] = Form.useForm();
   const init = useRequest<{ data: FormAPI.Data }>(uri);
 
   useEffect(() => {
     if (visible) {
+      form.resetFields();
       init.run();
     }
   }, [visible]);
+
+  const setFieldsValueAdapter = (data: FormAPI.Data) => {
+    if (data?.layout?.tabs && data?.dataSource) {
+      const newFieldValues = {};
+      data.layout.tabs.forEach((tab) => {
+        tab.data.forEach((field) => {
+          switch (field.type) {
+            case 'datetime':
+              newFieldValues[field.key] = moment(data.dataSource[field.key]);
+              break;
+            default:
+              newFieldValues[field.key] = data.dataSource[field.key];
+              break;
+          }
+        });
+      });
+      return newFieldValues;
+    }
+    return {};
+  };
+
+  useEffect(() => {
+    if (init.data) {
+      form.setFieldsValue(setFieldsValueAdapter(init.data));
+    }
+  }, [init.data]);
 
   return (
     <div>
@@ -28,8 +57,18 @@ const Modal = ({
         visible={visible}
         footer={ActionBuilder(init?.data?.layout?.actions[0].data)}
         onCancel={handleCancel}
+        maskClosable={false}
       >
-        <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form
+          form={form}
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{
+            create_time: moment(),
+            update_time: moment(),
+            status: true,
+          }}
+        >
           {FormBuilder(init?.data?.layout?.tabs[0].data)}
         </Form>
       </AntdModal>
--- src/pages/BasicList/builder/FormBuilder.tsx
+++ src/pages/BasicList/builder/FormBuilder.tsx
@@ -1,12 +1,39 @@
-import { Form, Input } from 'antd';
+import { DatePicker, Form, Input, Switch, TreeSelect } from 'antd';
 
 const FormBuilder = (data: FormAPI.Datum[] | undefined) => {
   return (data || []).map((field) => {
-    return (
-      <Form.Item key={field.key} label={field.title} name={field.key}>
-        <Input />
-      </Form.Item>
-    );
+    switch (field.type) {
+      case 'text': {
+        return (
+          <Form.Item key={field.key} label={field.title} name={field.key}>
+            <Input disabled={field.disabled} />
+          </Form.Item>
+        );
+      }
+      case 'tree': {
+        return (
+          <Form.Item key={field.key} label={field.title} name={field.key}>
+            <TreeSelect disabled={field.disabled} treeData={field.data} treeCheckable />
+          </Form.Item>
+        );
+      }
+      case 'datetime': {
+        return (
+          <Form.Item key={field.key} label={field.title} name={field.key}>
+            <DatePicker showTime disabled={field.disabled} />
+          </Form.Item>
+        );
+      }
+      case 'switch': {
+        return (
+          <Form.Item key={field.key} label={field.title} name={field.key} valuePropName={'checked'}>
+            <Switch disabled={field.disabled} />
+          </Form.Item>
+        );
+      }
+      default:
+        return null;
+    }
   });
 };
 
